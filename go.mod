c9cd87db7652d40f72546f4720c3dac9f88340d2
Martin Lukasik <martilukas7@gmail.com>
2021-01-18 10:18:54 +0100

    working osquery-server

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/mlukasik-dev/osquery-server
+
+go 1.15
+
+require (
+	github.com/apache/thrift v0.13.1-0.20200603211036-eac4d0c79a5f
+	github.com/gofiber/fiber v1.14.6
+	github.com/gofiber/fiber/v2 v2.3.3
+	github.com/pkg/errors v0.9.1
+)
---
+++ main.go
@@ -0,0 +1,52 @@
+package main
+
+import (
+	"flag"
+	"log"
+	"os"
+	"time"
+
+	"github.com/gofiber/fiber/v2"
+	"github.com/mlukasik-dev/osquery-server/pkg/osquery"
+)
+
+func main() {
+	socket := flag.String("socket", "", "Path to osquery socket file")
+	flag.Parse()
+	if *socket == "" {
+		log.Fatalf(`Usage: %s -socket SOCKET_PATH`, os.Args[0])
+	}
+
+	client, err := osquery.NewClient(*socket, time.Second*3)
+	if err != nil {
+		log.Fatalf("error: %v", err)
+	}
+	defer client.Close()
+
+	c := &controller{client}
+
+	app := fiber.New()
+
+	app.Get("/", c.handler)
+
+	app.Listen(":8080")
+}
+
+type controller struct {
+	client *osquery.ExtensionManagerClient
+}
+
+func (ctr *controller) handler(c *fiber.Ctx) error {
+	query := c.Query("q")
+	if query == "" {
+		return fiber.NewError(fiber.StatusBadRequest, "query must be provided")
+	}
+	resp, err := ctr.client.Query(query)
+	if err != nil {
+		return fiber.NewError(fiber.StatusInternalServerError, "error communicating with osqueryd: "+err.Error())
+	}
+	if resp.Status.Code != 0 {
+		return fiber.NewError(fiber.StatusInternalServerError, "osqueryd returned error: "+resp.Status.Message)
+	}
+	return c.JSON(resp.Response)
+}
